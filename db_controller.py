c8d5da29daa12d55ecfc9d65299a841a36d771fb
Louis Charles Almeida Maciel <louischarles7@hotmail.com>
2025-05-02 06:05:00 +0200

    Adiciona rotas Flask e integrações com banco para notas, metas e feedbacks

---
+++ db_controller.py
@@ -0,0 +1,9 @@
+# Arquivo criado somente para fins de teste, deve ser substituido pelo o arquivo db_controller.py correto
+def salvar_nota(aluno, valor):
+    print(f"Nota salva: {aluno} - {valor}")
+
+def salvar_feedback(aluno, texto):
+    print(f"Feedback salvo: {aluno} - {texto}")
+
+def salvar_meta(aluno, descricao):
+    print(f"Meta salva: {aluno} - {descricao}")
